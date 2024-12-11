0a76f87de4ae1495924ba854a8abefa380e08e21
Uğur Değirmenci <m.ugurdegirmenci@gmail.com>
2024-12-11 13:42:33 +0300

    evaluating our A3C agent on a certain number of episodes

--- main.py
+++ main.py
@@ -138,3 +138,18 @@ class Agent():
 
 #initializing the a3c agent
 agent = Agent(number_actions)
+
+#evaluating our A3C agent on a certain number of episodes
+def evaluate(agent, env, n_episodes = 1):
+   episodes_rewards = []
+   for _ in range(n_episodes):
+    state, _ = env.reset()
+    total_reward = 0
+    while True:
+       action = agent.act(state)
+       state, reward, done, info, _ = env.step(action[0])
+       total_reward += reward
+       if done:
+          break
+    episodes_rewards.append(total_reward)
+    return episodes_rewards
