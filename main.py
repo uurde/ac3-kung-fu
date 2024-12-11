d09768b659d34f40fcbf09cda217b712d084431a
Uğur Değirmenci <m.ugurdegirmenci@gmail.com>
2024-12-11 13:45:55 +0300

    managing multiple environments simultaneously

--- main.py
+++ main.py
@@ -153,3 +153,21 @@ def evaluate(agent, env, n_episodes = 1):
           break
     episodes_rewards.append(total_reward)
     return episodes_rewards
+   
+#managing multiple environments simultaneously
+class EnvBatch:
+    def __init__(self, n_envs = 10):
+       self.envs = [make_env() for _ in range(n_envs)]
+    
+    def reset(self):
+       _states = []
+       for env in self.envs: 
+          _states.append(env.reset()[0])
+       return np.array(_states)
+
+    def step(self, actions):
+        next_states, rewards, dones, infos, _ = map(np.array, zip(*[env.step(a) for env, a in zip(self.envs, actions)]))
+        for i in range(len(self.envs)): 
+           if dones[i]: 
+              next_states[i] = self.envs[i].reset()[0]
+        return next_states, rewards, dones, infos
