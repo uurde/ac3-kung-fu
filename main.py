f4f50d3148676cb04d6c0273407d49ad2bd94037
Uğur Değirmenci <m.ugurdegirmenci@gmail.com>
2024-12-11 13:29:35 +0300

    setting up environment

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ from gymnasium import ObservationWrapper
 #Creating the architecture of the Neural Network
 class Network(nn.Module):
     def __init__(self, action_size):
-        super(nn.Module, self).__init__()
-        self.conv1 = torch.nn.Conv2d(in_channels = 4, out_channels = 32, kernel_size = (3, 3), stride = 2)
-        self.conv2 = torch.nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = (3, 3), stride = 2)
-        self.conv3 = torch.nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = (3, 3), stride = 2)
+        super(Network, self).__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels = 4,  out_channels = 32, kernel_size = (3,3), stride = 2)
+        self.conv2 = torch.nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = (3,3), stride = 2)
+        self.conv3 = torch.nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = (3,3), stride = 2)
         self.flatten = torch.nn.Flatten()
         self.fc1 = torch.nn.Linear(512, 128)
         self.fc2a = torch.nn.Linear(128, action_size)
@@ -43,7 +43,6 @@ class Network(nn.Module):
 
 #setting up environment
 class PreprocessAtari(ObservationWrapper):
-
   def __init__(self, env, height = 42, width = 42, crop = lambda img: img, dim_order = 'pytorch', color = False, n_frames = 4):
     super(PreprocessAtari, self).__init__(env)
     self.img_size = (height, width)
@@ -98,3 +97,41 @@ print("Action names:", env.env.env.env.get_action_meanings())
 learning_rate = 10e-4
 discount_factor = 0.99
 number_envrionments = 10
+
+#implement the ac3 class
+class Agent():
+    def __init__(self, action_size):
+       self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+       self.action_size = action_size
+       self.network = Network(action_size).to(self.device)
+       self.optimizer = torch.optim.Adam(self.network.parameters(), lr = learning_rate)
+
+    def act(self, state):
+        if state.ndim == 3:
+            state = [state]
+        state = torch.tensor(state, dtype = torch.float32, device = self.device)
+        action_values, _ = self.network(state)
+        policy = F.softmax(action_values, dim = -1)
+        return np.array([np.random.choice(len(p), p = p) for p in policy.detach().cpu().numpy()])
+
+    def step(self, state, action, reward, next_state, done):
+        batch_size = state.shape[0]
+        state = torch.tensor(state, dtype = torch.float32, device = self.device)
+        next_state = torch.tensor(next_state, dtype = torch.float32, device = self.device)
+        reward = torch.tensor(reward, dtype = torch.float32, device = self.device)
+        done = torch.tensor(done, dtype = torch.bool, device = self.device).to(dtype = torch.float32)
+        action_values, state_value = self.network(state)
+        _, next_state_value = self.network(next_state)
+        target_state_value = reward + discount_factor * next_state_value * (1 - done)
+        advantage = target_state_value - state_value
+        probs = F.softmax(action_values, dim = -1)
+        logprobs = F.log_softmax(action_values, dim = -1)
+        entropy = -torch.sum(probs * logprobs, axis = -1)
+        batch_idx = np.arange(batch_size)
+        logp_actions = logprobs[batch_idx, action]
+        actor_loss = -(logp_actions * advantage.detach()).mean() - 0.001 * entropy.mean()
+        critic_loss = F.mse_loss(target_state_value.detach(), state_value)
+        total_loss = actor_loss + critic_loss
+        self.optimizer.zero_grad()
+        total_loss.backward()
+        self.optimizer.step()
